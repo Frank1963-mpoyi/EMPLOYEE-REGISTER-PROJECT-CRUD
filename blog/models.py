5302a2290ce6b3c0a2f521b7425f0de909a4be93
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-05-16 12:52:46 +0200

    slug generator

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
-from    django.db               import          models
-from    core.model_mixins       import          AuditFields 
-
-from    django.urls             import          reverse
-
+from    django.db                   import          models
+from    core.model_mixins           import          AuditFields 
+from    blog.utils                  import          unique_slug_generator
+from    django.urls                 import          reverse
+from    django.db.models.signals    import          pre_save, post_save
 
 
 
@@ -23,7 +23,13 @@ class Employee(AuditFields):
     empl_code   = models.CharField('EMPLOYEE CODE',  max_length=3)
     mobile      = models.CharField('MOBILE',         max_length=15)
     position    = models.ForeignKey(Position,       on_delete=models.CASCADE , null=True, blank=True)
-    slug        = models.SlugField()
+    slug        = models.SlugField(blank=True, unique=True)
+
+
+    @property
+    def title(self):
+        return "title" # this is add the field and value to the model 
+
 
     def __str__(self):
         return self.fullname
@@ -38,3 +44,10 @@ class Employee(AuditFields):
         return reverse("blog:delete", kwargs={"slug": self.slug})
     
     
+    
+
+#this is the method to handle pre_save
+def employee_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+pre_save.connect(employee_pre_save_receiver,  sender= Employee)
