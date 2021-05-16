5537b577dc0e45631de912b20c2c8790a2b443f0
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-05-16 11:26:34 +0200

    try to handle exceptions

--- blog/views.py
+++ blog/views.py
@@ -18,33 +18,40 @@ def employee_list (request):
 
 def employee_form (request):
     template_name      = 'blog/employee_form.html'
-    if request.method == "GET":
-        form    = EmployeeForm()
-        context =  {'form': form }
-        return render (request, template_name, context )
-    else:
-        form    = EmployeeForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-        return redirect('blog:list')
-        
+    try:
+        if request.method == "GET":
+            form    = EmployeeForm()
+            context =  {'form': form }
+            return render (request, template_name, context )
+        else:
+            form    = EmployeeForm(request.POST or None)
+            if form.is_valid():
+                form.save()
+            return redirect('blog:list')
+    except:
+        return redirect('blog:list')           
         
     
 def updat_form (request, slug=None):
     template_name = 'blog/employee_form.html'
-    obj     = get_object_or_404(Employee, slug=slug)
-    form    = EmployeeForm(request.POST or None , instance=obj)
-    if form.is_valid():
-            form.save()
-            return redirect('blog:list')
-    context =  {'form': form }
-    return render (request, template_name, context )
-
+    try:
+        obj     = get_object_or_404(Employee, slug=slug)
+        form    = EmployeeForm(request.POST or None , instance=obj)
+        if form.is_valid():
+                form.save()
+                return redirect('blog:list')
+        context =  {'form': form }
+        return render (request, template_name, context )
+    except:
+        return redirect('blog:list')    
 
 
 def employee_delete (request, slug=None):
     template_name = 'blog/index.html'
-    obj = get_object_or_404(Employee, slug=slug)
-    obj.delete()
-    return redirect('blog:list')
-    #return redirect(employee_list)
+    try:
+        obj = get_object_or_404(Employee, slug=slug)
+        obj.delete()
+        return redirect('blog:list')
+        #return redirect(employee_list)
+    except:
+        return redirect('blog:list') 
